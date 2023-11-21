e8721a0ebed6c7f5ab0869581d49c83a23881389
Cesar laptop <udemicscesar@gmail.com>
2023-11-21 16:27:58 -0400

    mas reservasss

--- database/seeders/ReservaSeeder.php
+++ database/seeders/ReservaSeeder.php
@@ -14,12 +14,48 @@ class ReservaSeeder extends Seeder
     public function run(): void
     {
         Reserva::create([
-            'fecha' => '2023-11-10',
+            'fecha' => '2023-11-23',
             'hora_inicio' => '10:00:00',
-            'hora_fin' => '11:00:00',
-            'estado' => 'No aprobado',
-            'servicio_id' => '4',
+            'hora_fin' => '10:29:59',
+            'estado' => 'Aprobado',
+            'servicio_id' => '1',
             'cliente_id' => '2',
+            'empleado_id' => '1'
+        ]);
+        Reserva::create([
+            'fecha' => '2023-11-23',
+            'hora_inicio' => '10:30:00',
+            'hora_fin' => '11:59:59',
+            'estado' => 'Aprobado',
+            'servicio_id' => '3',
+            'cliente_id' => '3',
+            'empleado_id' => '1'
+        ]);
+        Reserva::create([
+            'fecha' => '2023-11-23',
+            'hora_inicio' => '14:00:00',
+            'hora_fin' => '14:29:59',
+            'estado' => 'Aprobado',
+            'servicio_id' => '1',
+            'cliente_id' => '4',
+            'empleado_id' => '1'
+        ]);
+        Reserva::create([
+            'fecha' => '2023-11-24',
+            'hora_inicio' => '14:00:00',
+            'hora_fin' => '14:29:59',
+            'estado' => 'Aprobado',
+            'servicio_id' => '1',
+            'cliente_id' => '2',
+            'empleado_id' => '1'
+        ]);
+        Reserva::create([
+            'fecha' => '2023-11-23',
+            'hora_inicio' => '14:30:00',
+            'hora_fin' => '14:59:59',
+            'estado' => 'Aprobado',
+            'servicio_id' => '1',
+            'cliente_id' => '1',
             'empleado_id' => '1'
         ]);
     }
