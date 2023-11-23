8bd01bb36a8105eaa0f7877781fd5c43e6cff25b
CesarKnight <cesarcaballero111@gmail.com>
2023-11-22 23:02:39 -0400

    reserva seeder añadida

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,7 +36,7 @@ class DatabaseSeeder extends Seeder
             FacturaSeeder::class,
             PagoSeeder::class,
             OrdenDeTrabajoSeeder::class,
-            // ReservaSeeder::class,
+            ReservaSeeder::class,
         ]);
     }
 }
