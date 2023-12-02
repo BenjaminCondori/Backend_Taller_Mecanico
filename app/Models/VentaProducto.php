792002c049144d155024733c7175c3a18460e1ba
CesarKnight <cesarcaballero111@gmail.com>
2023-12-02 15:39:41 -0400

    modelo ventaproductoerror

--- app/Models/VentaProducto.php
+++ app/Models/VentaProducto.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class venta_producto extends Model
+class VentaProducto extends Model
 {
     use HasFactory;
     protected $table = 'venta_producto';
